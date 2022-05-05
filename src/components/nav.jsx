6d1c00f4c5e8e6c96928049b3205b33362ee3fba
Van Hien <hiench2001@gmail.com>
2022-05-04 20:09:36 -0600

    updated

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,3 +1,77 @@
+import { Link } from "react-scroll";
+import React, { useState } from "react";
+import { Sling as Hamburger } from "hamburger-react";
+
 export const Nav = () => {
-  return <h1>Test</h1>;
+  const [open, setOpen] = useState(false);
+  const [close, setClose] = useState(false);
+
+  return (
+    <section className="nav">
+      <div className="container">
+        <h2>Van Hien Tieu</h2>
+        <Hamburger
+          size={25}
+          color="white"
+          rounded
+          toggled={close}
+          toggle={setClose}
+          aria-expanded={close}
+          onToggle={(toggled) => {
+            if (toggled) {
+              setOpen(true);
+            } else {
+              setOpen(false);
+            }
+          }}
+        />
+        <div className={`nav-links ${open ? "" : "closed"}`}>
+          <Link
+            to="home"
+            className="link"
+            smooth={true}
+            offset={-10}
+            spy={true}
+            duration={700}
+            onClick={() => {
+              setClose(false);
+              setOpen(false);
+            }}
+          >
+            Home
+          </Link>
+          <Link
+            d
+            className="link"
+            to="about"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={700}
+            onClick={() => {
+              setClose(false);
+              setOpen(false);
+            }}
+          >
+            About
+          </Link>
+
+          <Link
+            className="link"
+            to="location"
+            spy={true}
+            smooth={true}
+            offset={5}
+            duration={1200}
+            onClick={() => {
+              setClose(false);
+              setOpen(false);
+            }}
+          >
+            Contact
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
 };
