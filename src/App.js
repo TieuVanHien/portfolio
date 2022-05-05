bd42754d6526e87be1d341fa9449a323886a894e
Van Hien <hiench2001@gmail.com>
2022-05-05 08:38:14 -0600

    completed nav bar and landing page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Nav } from "./components";
+import { Home } from "./sections";
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Home />
     </div>
   );
 }
--- src/sections/home.js
+++ src/sections/home.js
@@ -1,7 +1,24 @@
+import TypingEffect from "typing-effect-react";
+import { background } from "../assets";
+
 export const Home = () => {
   return (
     <section className="home">
-      <div className="container"></div>
+      <div className="container">
+        <img src={background} alt="background home page" />
+        <div className="home headings">
+          <h1>
+            <span style={{ color: "#F4DB7D" }}> Hello</span>, I'm Van Hien Tieu
+          </h1>
+          <h1>
+            I'm a{" "}
+            <TypingEffect
+              className="home heading-effect"
+              data={["Front End Developer", "Webflow Developer"]}
+            />
+          </h1>
+        </div>
+      </div>
     </section>
   );
 };
