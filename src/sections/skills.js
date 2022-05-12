15ff97f010dee673065a16f430ec6feb288850ad
Van Hien <hiench2001@gmail.com>
2022-05-11 22:57:10 -0600

    added skills images and skills section

---
+++ src/sections/skills.js
@@ -0,0 +1,10 @@
+export const Skills = () => {
+  return (
+    <section className="skill">
+      <div className="container">
+        <h1>Here Are Some Of My Skills</h1>
+        <div className="skill list"></div>
+      </div>
+    </section>
+  );
+};
